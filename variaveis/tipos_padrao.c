3e40d150431c4720e960775a991d9c586f5e116f
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-06-25 19:35:38 -0300

    10° commit - atualizando especificadores de formato

--- variaveis/tipos_padrao.c
+++ variaveis/tipos_padrao.c
@@ -11,11 +11,13 @@ int main(){
 	float Float;
 	double Double;
 	
-	printf("Tipos de variaveis | Tamanho (Byts) |            Intervalo\n");
-	printf("       char        |        %d       |           %d a %d\n", sizeof(char), (int) - pow(2, sizeof(char) * 8 - 1), (int)pow(2,sizeof(char) * 8 - 1) - 1);
-	printf("       int         |        %d       |    %d a %d\n", sizeof(int), (int) - pow(2, sizeof(int) * 8 - 1), (int) pow(2, sizeof(int) * 8 - 1));
-	printf("      float        |        %d       | %e a %e\n", sizeof(float), -pow(2, -126), pow(2, 128));
-	printf("      double       |        %d       | %e a %e\n", sizeof(double) , -pow(2, -1022), (2.0 - pow(2, -52)) * pow(2, 1023));
+	printf("Tipos de variaveis | Especificadores | Tamanho (Byts) |            Intervalo\n");
+	printf("                   |   de formato    |                |\n");
+	printf("-------------------|-----------------|----------------|--------------------------------\n");
+	printf("       char        |     %%c ou %%s    |        %zu       |           %d a %d  \n", sizeof(char), (int) - pow(2, sizeof(char) * 8 - 1), (int)pow(2,sizeof(char) * 8 - 1) - 1);
+	printf("       int         |     %%d ou %%i    |        %zu       |    %d a %d         \n", sizeof(int), (int) - pow(2, sizeof(int) * 8 - 1), (int) pow(2, sizeof(int) * 8 - 1));
+	printf("      float        |        %%f       |        %zu       | %e a %e            \n", sizeof(float), -pow(2, -126), pow(2, 128));
+	printf("      double       |       %%lf       |        %zu       | %e a %e            \n", sizeof(double) , -pow(2, -1022), (2.0 - pow(2, -52)) * pow(2, 1023));
 	
   return 0;
 }

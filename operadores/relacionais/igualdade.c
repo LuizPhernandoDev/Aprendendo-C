d579913617884f82169b7d743ce4661561fc10f3
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-06-28 17:38:28 -0300

    25° commit - correção e atualização dos operadores relacionais

--- operadores/relacionais/igualdade.c
+++ operadores/relacionais/igualdade.c
@@ -13,7 +13,11 @@ int main(){
 	printf("%d\n", 0.0 == 0.1);   //imprime 0
 	
 	printf("%d\n", 'A' == 65);    //imprime 1 ==> letras (char) são armazenados como números inteiros de 1byte
+	printf("%d\n", 'a' == 97);    //imprime 1 
 	printf("%d\n", 'A' == 'a');   //imprime 0 ==> letras maiúsculas são diferentes de minúsculas
+	printf("%d\n", 'A' == 97);    //imprime 0
+	printf("%d\n", 65 == 'a');    //imprime 0
+	printf("%d\n", 65 == 97);     //imprime 0
 	
     return 0;
 }
--- operadores/relacionais/menor_igual.c
+++ operadores/relacionais/menor_igual.c
@@ -2,18 +2,22 @@
 
 int main(){
 
-	printf("%d\n", 0 >= 0);       //imprime 1
-	printf("%d\n", 0.0 >= 0.0);   //imprime 1
-	printf("%d\n", 0.0 >= 0);     //imprime 1
-	printf("%d\n", 0 >= 0.0);     //imprime 1
+	printf("%d\n", 0 <= 0);       //imprime 1
+	printf("%d\n", 0.0 <= 0.0);   //imprime 1
+	printf("%d\n", 0.0 <= 0);     //imprime 1
+	printf("%d\n", 0 <= 0.0);     //imprime 1
 	
-	printf("%d\n", 1 >= 0);       //imprime 1 
-	printf("%d\n", 0 >= 1);       //imprime 0 
-	printf("%d\n", 0.1 >= 0.0);   //imprime 1
-	printf("%d\n", 0.0 >= 0.1);   //imprime 0
-	
-	printf("%d\n", 'A' >= 65);    //imprime 1 ==> letras (char) são armazenados como números inteiros de 1byte
-	printf("%d\n", 'A' >= 'a');   //imprime 0 ==> letras maiúsculas são diferentes de minúsculas
+	printf("%d\n", 1 <= 0);       //imprime 0 
+	printf("%d\n", 0 <= 1);       //imprime 1 
+	printf("%d\n", 0.1 <= 0.0);   //imprime 0
+	printf("%d\n", 0.0 <= 0.1);   //imprime 1
+
+	printf("%d\n", 'A' <= 65);    //imprime 1 ==> letras (char) são armazenados como números inteiros de 1byte
+	printf("%d\n", 'a' <= 97);    //imprime 1 
+	printf("%d\n", 'A' <= 'a');   //imprime 1 ==> letras maiúsculas são diferentes de minúsculas
+	printf("%d\n", 'A' <= 97);    //imprime 1
+	printf("%d\n", 65 <= 'a');    //imprime 1
+	printf("%d\n", 65 <= 97);     //imprime 1
 	
     return 0;
 }

32b437f8d203c45fdb7e3c06d3844db27d197b09
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-06-29 21:48:57 -0300

    46° commit - adicionando matrizes

---
+++ vetor/matriz1_inicializando.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	int tabela[3][4];   // Matriz de números inteiros com 3 linhas e 4 colunas
+	float notas[5][2];  // Matriz com 5 linhas (ex: alunos) e 2 colunas (ex: provas)
+    
+    return 0;
+}
---
+++ vetor/matriz2_declarando.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int matriz[2][3] = {{10, 20, 30}, // Primeira linha (índice 0)
+    					{40, 50, 60}  // Segunda linha (índice 1)
+    								};
+
+    return 0;
+}

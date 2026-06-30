186b743606cba2f94130f310c40afedb85270b91
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-06-30 09:39:46 -0300

    51° commit - alterando arrays

---
+++ variaveis/Arrays/matriz/matriz2_inicializando.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int matriz[3][3] = {{1, 2, 3},  // Primeira linha (índice 0)
+    					{4, 5, 6};  // Segunda linha (índice 1)
+						{7, 8, 9}   // terceira linha (índice 2)
+    							 };
+								 
+    return 0;
+}
---
+++ variaveis/Arrays/matriz/matriz4_for.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){	
+	int mat[3][3], i, j;
+	
+	for(i = 0;i < 3; i++){
+		for(j = 0; j < 3; j++){
+			mat[i][j]= i*3 +j+1;
+		}
+	}
+	for(i = 0;i < 3; i++){
+		for(j = 0; j < 3; j++){
+			printf("%d ",mat[i][j]);
+		}
+		printf("\n");
+
+        return 0;
+	}
+}

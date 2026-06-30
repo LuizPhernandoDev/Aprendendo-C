4cd808ea9e5503ae30023263409a2e989f1a2438
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-06-29 21:51:11 -0300

    47° commit adicionando comentario

--- vetor/matriz1_inicializando.c
+++ vetor/matriz1_inicializando.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//pode se ter n linha e colunas ,ex: matriz[i][j][k][l]
 int main() {
 	int tabela[3][4];   // Matriz de números inteiros com 3 linhas e 4 colunas
 	float notas[5][2];  // Matriz com 5 linhas (ex: alunos) e 2 colunas (ex: provas)

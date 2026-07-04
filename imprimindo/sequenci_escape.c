4e9a70b82740fcf0f86164f459518dd829cbe154
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-03 21:33:39 -0300

    65° commit - adicionando sequencias de escapes

---
+++ imprimindo/sequenci_escape.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("Pula linha:                          \n");   
+	printf("Toca um bipe: \a                     \n");   
+	printf("Backspace: oi\b                      \n");   
+	printf("Tabulacao horizontal: \t oi          \n");   
+	printf("Retorna ao inicio da linha: \r Volta \n"); 
+	printf("Tabulacao vertical: \v oi            \n"); 
+	printf("Caracter: %%                         \n");   
+	printf("Caracter: \\                         \n");     
+	printf("Caracter: \'                         \n");   
+	printf("Caracter: \"                         \n");   
+	printf("Caracter: \?                         \n"); 
+	printf("Caracter: \"                         \n");   
+	printf("Caracter: \?                         \n");
+	printf("Caracter: \101 em octal              \n");   
+	printf("Caracter: \x41 em hexadecimal        \n");    
+	
+	return 0;
+}

fcabd578fa5a094600627a77e1065d35e390d2af
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-18 11:27:03 -0300

    109° commit - adicionando funções de arrendodamento da biblioteca math.h

---
+++ bibliotecas/matc.h/arredondamento.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x = 3.1, y = 4.9;
+    double a = 3.5, b = 0.5, c;
+    
+    printf("Arredonda o valor para cima, para o próximo número inteiro: %.1lf\n", ceil(x));
+    printf("Arredonda o valor para baixo, para o inteiro anterior: %.1lf\n", floor(y));
+    printf("Corta a parte decimal inteira, deixando apenas o inteiro: %.1lf\n", trunc(y));
+    printf("Arredonda para o inteiro mais próximo (matemática comum): %.1lf\n", round(a));
+    printf("Retorna o resto da divisão entre dois números decimais: %.1lf\n", fmod(a, b));
+    
+    double fracao = modf(a, &c);  // fix fora do printf para ter certexa que o compilador iria executar a funcao primeiro
+    
+    printf("Separa e retorna a fração, gravando o inteiro no ponteiro c: decimal = %.1lf e inteiro = %.1lf\n", fracao, c);
+
+    return 0;
+}

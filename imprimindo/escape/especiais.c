0f2038e21de233918f4780e202695f692dadf08e
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-24 18:42:26 -0300

    136° commit - adicionando caracteres especiais

---
+++ imprimindo/escape/especiais.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//esses caracteres sem a biblioteca locale imprime outros caracteres 
+int main(){
+    printf("¯½æø§Ð¨÷¸©®õôçÑÿáúèùïÏ\n\n");
+	printf("Retas: ¼ªº¹³ð¿Ã»îÂÊÎÙÀÈÌÄËòÚÁÉÍ´Ý\n\n");
+	printf("Massivo: ²°±þßÛÜ\n\n");
+	printf("Letras: ¥¡·äëÒìàÓéíÇÔêâã¤£¢Ø×ƒÞ¶µÆÖÕ\n\n");
+	printf("Numero: ¦¬öüóýñû«");
+}

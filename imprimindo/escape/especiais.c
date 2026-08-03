361d4473457c32c30ebfb9bcb730c01473950bb2
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-08-03 16:11:45 -0300

    140° commit - alterando caracteres especiais

--- imprimindo/escape/especiais.c
+++ imprimindo/escape/especiais.c
@@ -2,9 +2,27 @@
 
 //esses caracteres sem a biblioteca locale imprime outros caracteres 
 int main(){
-    printf("¯½æø§Ð¨÷¸©®õôçÑÿáúèùïÏ\n\n");
-	printf("Retas: ¼ªº¹³ð¿Ã»îÂÊÎÙÀÈÌÄËòÚÁÉÍ´Ý\n\n");
+    printf("¯½æø§Ð¨÷¸©®õôçÑÿáúèùïÏœ\n\n");
+	printf("Retas: ¼ªº¹³ð¿Ã»îÂÊÎÙÀÈÌÄËòÚÁÉÍ´ÝÅ\n\n");
 	printf("Massivo: ²°±þßÛÜ\n\n");
-	printf("Letras: ¥¡·äëÒìàÓéíÇÔêâã¤£¢Ø×ƒÞ¶µÆÖÕ\n\n");
-	printf("Numero: ¦¬öüóýñû«");
+	
+	printf("Letras: \n\n");
+	
+	printf("Maiusculas: \n");
+	printf("Grave: ·ÔÞãë\n");
+	printf("Trema: ŽÓØ™š\n");
+	printf("Agudo: íµÖàé\n");
+	printf("Til: ¥Çå\n");
+	printf("cirunflexo: ¶Ò×âê\n");
+	printf("Outros: \n\n");
+	
+	printf("Minusculas: \n");
+	printf("Grave: …Š•\n");
+	printf("Trema: ˜„‰‹”\n");
+	printf("Agudo: ì‚¡¢£\n");
+	printf("Til: ¤Æä\n");
+	printf("cirunflexo: ƒŒ“–\n");
+	printf("Outros: †‡Õ›\n\n");
+	
+	printf("Numero: ¦¬öüóýñû«ž");
 }
